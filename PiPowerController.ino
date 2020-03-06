01be737c8f732b15783b22f41110d26102659873
zybeon <yates.rodney@gmail.com>
2020-03-06 16:36:56 -0500

    Update PiPowerController.ino

--- PiPowerController.ino
+++ PiPowerController.ino
@@ -1,160 +1,163 @@
-int PI_POWER = 4; // Default is Pin27? "dtoverlay=gpio-poweroff,gpiopin=2,active_low="y"
-int PI_SHDWN = 3; //Default is GPIO3 on Rpi with "dtoverlay=gpio-shutdown"
-int RELAY_PIN = 0;
-int BUTTON_PIN = 2;
+int PI_POWER = 3; // Default is Pin27? "dtoverlay=gpio-poweroff,gpiopin=2,active_low="y"
+int PI_SHDWN = 4; //Default is GPIO3 on Rpi with "dtoverlay=gpio-shutdown"
+int RELAY_PIN = 2;
+int BUTTON_PIN = 0;
 int LED_PIN = 1;
-int AUTOPWR = 0; // 0 for auto off | 1 for auto on when plugged in
-int MOM_TOGGLE = 0; // 0 for momentary | 1 for toggle
-int RELAY_MODE = 0; // 0 for LOW on | 1 for HIGH on
+
+bool AUTOPWR = 0; // 0 for auto off | 1 for auto on when plugged in
+bool MOM_TOGGLE = 1; // 1 for momentary | 0 for toggle
+bool RELAY_MODE = 0; // 0 for LOW on | 1 for HIGH on
 
 int checkOffCount = 0;
 int waitingOffTimeoutCount=0;
-int powerstatus = 0; 
-//Power Toggle Mode
+int powerstatus; 
+//Power Modes
 // 0 Power off
-// 1 Powering On (not used for now)
-// 2 Powered on 
-// 3 Powering off
-// 4 Waiting for Safe Shutdown to finish
-// 5 Hard Shutdown
+// 1 Power on 
+// 2 Powering off
+// 3 Waiting for Pi to shutdown
+// 4 Hard Shutdown
 
 int buttondowncount = 0;
 int buttonupcount = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-    pinMode(LED_PIN, OUTPUT); //LED 
-    pinMode(RELAY_PIN, OUTPUT); //Power Relay
-    pinMode(BUTTON_PIN, INPUT);  //Button
-    pinMode(PI_SHDWN, OUTPUT); //Shutdown trigger
-    //pinMode(PI_POWER, INPUT); //Sense Pi Power
-    
-    if(AUTOPWR)//setting auto power status on power up.
-      powerstatus = 1;
-    else
-      powerstatus = 0;
+void setup(){
+	// put your setup code here, to run once:
+	pinMode(LED_PIN, OUTPUT); //LED 
+	pinMode(RELAY_PIN, OUTPUT); //Power Relay
+	pinMode(BUTTON_PIN, INPUT);  //Button
+	pinMode(PI_SHDWN, OUTPUT); //Shutdown trigger
+	pinMode(PI_POWER, INPUT); //Sense Pi Power
+	
+  if(AUTOPWR)//Setting auto power status on power up.
+		powerstatus = 1;
+	else
+		powerstatus = 0;
 }
 
 void loop() {
-      //Read the state of the switch into a local variable:
-      bool reading = digitalRead(BUTTON_PIN);
 
-      if(buttondowncount>1000)
-        buttondowncount = 0;
-        
-      if(buttonupcount>1000)
-        buttonupcount = 0;
-
-      if (!reading) {
-        // Reset the debouncing timer
-        buttonupcount = 0;
-        buttondowncount = buttondowncount + 1;       
-      } 
-
-      if (reading) {
-        buttonupcount = buttonupcount + 1;
-
-        
-        if(buttondowncount>=60 && buttonupcount >2)
-        {//Long Pressed Button
-          buttondowncount = 0;
-          buttonupcount = 0;
-          powerstatus = 5;
-        }
-        else if(buttondowncount>=3 && buttonupcount >2)
-        {//Button Pressed
-          buttondowncount = 0;
-          buttonupcount = 0;
-          if(powerstatus==0)
-            powerstatus=2;
-          else if(powerstatus==2)
-            powerstatus=3;            
-        }
+  //Read the state of the button/swtich into a local variable:
+	bool reading = !digitalRead(BUTTON_PIN);
+	
+	if(MOM_TOGGLE) //Mommentary button check
+	{
+		
+		if(buttondowncount>1000)
+			buttondowncount = 0;
+		if(buttonupcount>1000)
+			buttonupcount = 0;
+		
+		if (reading) {
+			// Reset the debouncing timer
+			buttonupcount = 0;
+			buttondowncount = buttondowncount + 1;
+		}
+		if (!reading)
+		{
+			buttonupcount = buttonupcount + 1;
+			if(buttondowncount>=60 && buttonupcount >2)
+			{//Long Pressed Button
+				buttondowncount = 0;
+				buttonupcount = 0;
+				powerstatus = 4;
+			}
+			else if(buttondowncount>=3 && buttonupcount >2)
+			{//Button Pressed
+				buttondowncount = 0;
+				buttonupcount = 0;
+				if(powerstatus==0)
+					powerstatus=1;
+				else if(powerstatus==1)
+					powerstatus=2;
+			}
+		}
+	}
+  else
+  {
+    if(powerstatus==0){
+      if(reading)
+        checkOffCount = checkOffCount + 1;
+      else
+        checkOffCount=0;
+      if(checkOffCount>=5) {
+        powerstatus = 1;
+        checkOffCount = 0;
       }
-      
-      if(powerstatus==1)
-      {
-          digitalWrite(LED_PIN, LOW);
-          delay(300);
-          digitalWrite(LED_PIN, HIGH);
-          delay(200);
-          digitalWrite(LED_PIN, LOW);
-          delay(300);
-          digitalWrite(LED_PIN, HIGH);
-          delay(200);
+    }
+    else if(powerstatus==1){
+      if(reading)
+        checkOffCount = checkOffCount + 1;
+      else
+      checkOffCount=0;
+      if(checkOffCount>=5){
+        powerstatus = 2;
+        checkOffCount = 0;
       }
-      else if(powerstatus==2)
-      {
-          digitalWrite(SHUTDOWN_PIN, LOW);
-          digitalWrite(LED_PIN, HIGH);
-          if(RELAY_MODE)
-            digitalWrite(RELAY_PIN, HIGH); 
-          else
-            digitalWrite(RELAY_PIN, LOW);
-      }
-      else if(powerstatus==3)
-      {
-          //Start Shutdown Sequence
-          // Call 
-          digitalWrite(SHUTDOWN_PIN, HIGH);
-          delay(200);
-          digitalWrite(SHUTDOWN_PIN, LOW);
-          powerstatus=4;
-          checkOffCount = 0;
-          waitingOffTimeoutCount = 0;
-      }
-      else if(powerstatus==4)
-      {
-          digitalWrite(LED_PIN, LOW);
-          delay(300);
-          digitalWrite(LED_PIN, HIGH);
-          delay(200);
-
-          //Check if it's off 5 times before shutting down                
-          if(checkPiOff())
-            checkOffCount = checkOffCount + 1;
-          else
-          {
-            checkOffCount=0;
-            waitingOffTimeoutCount = waitingOffTimeoutCount + 1;
-          }
-          
-          if(checkOffCount>=5)
-          {
-              //It's really off but wait 2 seconds
-              delay(2000);
-              digitalWrite(LED_PIN, LOW);
-              if(RELAY_MODE)
-                digitalWrite(RELAY_PIN, LOW); 
-          	  else
-            	digitalWrite(RELAY_PIN, HIGH); 
-              powerstatus = 0;
-              checkOffCount = 0;
-              waitingOffTimeoutCount = 0; 
-          }
-          else if(waitingOffTimeoutCount>=40)
-          {
-            //Time out shutdown anyway
-            digitalWrite(LED_PIN, LOW);
-            if(RELAY_MODE)
-              digitalWrite(RELAY_PIN, LOW); 
-            else
-              digitalWrite(RELAY_PIN, HIGH); 
-            powerstatus = 0;
-            checkOffCount = 0;
-            waitingOffTimeoutCount = 0;                        
-          }       
-      }
-      else if(powerstatus==5) //Hard shutdown
-      {
-          digitalWrite(LED_PIN, LOW);
-          if(RELAY_MODE)
-            digitalWrite(RELAY_PIN, LOW); 
-          else
-            digitalWrite(RELAY_PIN, HIGH);
-          powerstatus = 0;
-          checkOffCount = 0;
-      }
-      delay(50);
-      
+    }
+  }
+	if(powerstatus==0) //Power off
+	{
+		digitalWrite(LED_PIN, LOW);
+		if(RELAY_MODE)
+			digitalWrite(RELAY_PIN, LOW);
+		else
+			digitalWrite(RELAY_PIN, HIGH);
+	}
+	else if(powerstatus==1) //Power on
+	{
+		digitalWrite(PI_SHDWN, LOW);
+		digitalWrite(LED_PIN, HIGH);
+		if(RELAY_MODE)
+			digitalWrite(RELAY_PIN, HIGH);
+		else
+			digitalWrite(RELAY_PIN, LOW); 
+	}
+  else if(powerstatus==2) //Powering off
+  {
+    digitalWrite(PI_SHDWN, HIGH);
+    delay(200);
+    digitalWrite(PI_SHDWN, LOW);
+    powerstatus=3;
+    checkOffCount = 0;
+    waitingOffTimeoutCount = 0;
+  }
+  else if(powerstatus==3) //Waiting for Pi to shutdown
+  {
+    digitalWrite(LED_PIN, LOW);
+    delay(300);
+    digitalWrite(LED_PIN, HIGH);
+    delay(200);
+    
+    //Check if it's off 5 times before shutting down                
+    //if(checkPiOff())
+    if(!digitalRead(PI_POWER))
+      checkOffCount = checkOffCount + 1;
+    else
+    {
+      checkOffCount=0;
+      waitingOffTimeoutCount = waitingOffTimeoutCount + 1;
+    }
+    if(checkOffCount>=5)
+    {
+      //It's really off but wait 2 seconds
+      delay(2000);
+      powerstatus = 0;
+      checkOffCount = 0;
+      waitingOffTimeoutCount = 0; 
+    }
+    else if(waitingOffTimeoutCount>=40)
+    {
+      //Time out shutdown anyway
+      powerstatus = 0;
+      checkOffCount = 0;
+      waitingOffTimeoutCount = 0;                        
+    }
+  }
+  else if(powerstatus==4) //Hard shutdown
+  {
+    powerstatus = 0;
+		checkOffCount = 0;
+  }
+	delay(50);
 }
